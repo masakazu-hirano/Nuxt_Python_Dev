340e6efee69302e4542429795ec3034e9fb8c45f
masakazu-hirano <63486456+masakazu-hirano@users.noreply.github.com>
2023-04-14 16:52:16 +0900

    useAsyncData() 使用方法

---
+++ src/frontend/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>Hello World!</p>
+</template>
+
+<script setup>
+    const { data, pending, error, refresh } = await useAsyncData('Key', () => $fetch('URL', {
+        method: 'HTTPメソッド',
+        headers: {'': ''},
+        params: {'': ''}
+    }))
+
+    console.log(data.value)
+</script>
